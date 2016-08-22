4f46233f6ca11629dd18d55c44247a673245e2f5
yoyohoo <sinful_001@sina.com>
2016-08-22 17:56:25 +0800

    Update json.js

--- js/server/json.js
+++ js/server/json.js
@@ -2,17 +2,32 @@
  * @Author: xz06213
  * @Date:   2016-08-15 17:14:15
  * @Last Modified by:   xz06213
- * @Last Modified time: 2016-08-22 16:33:43
+ * @Last Modified time: 2016-08-22 17:48:48
  */
 
 'use strict';
 var fs = require('fs');
-exports.writeJson = function(filepath, data, callback) {
-	// fs.writeFile(filePath, data, function(err) {
-	// 	if (err) {
-	// 		res.success = false;
-	// 		res.message = ex;
-	// 	}
-	// });
-	fs.writeFile(filepath, data, 'utf8', callback);
+exports.readJson = function(filePath) {
+	var result = {
+		success: true
+	};
+	try {
+		result.data = fs.readFileSync(filePath, 'utf8');
+	} catch (ex) {
+		result.success = false;
+		result.message = ex;
+	}
+	return result;
+}
+exports.writeJson = function(filePath, data) {
+	var result = {
+		success: true
+	};
+	try {
+		fs.writeFileSync(filePath, JSON.stringify(data), 'utf8');
+	} catch (ex) {
+		result.success = false;
+		result.message = ex;
+	}
+	return result;
 }
